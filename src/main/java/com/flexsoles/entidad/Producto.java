7b20b20dbfe4b3424a615dc3adb03a9d74505f9e
mohaokrich <mohaokrich99@gmail.com>
2022-03-17 18:07:34 +0100

    fix 2

--- src/main/java/com/flexsoles/entidad/Producto.java
+++ src/main/java/com/flexsoles/entidad/Producto.java
@@ -17,9 +17,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import org.hibernate.annotations.NaturalIdCache;
 
-@Entity(name = "Producto")
+@Entity
 @Table(name = "producto")
-@NaturalIdCache
 public class Producto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
